ed088f1911e2ca622950505d894a8a21b73b0f7b
nomadop <nomadop@gmail.com>
2014-10-22 15:00:06 +0800

    Update simple_proxy.rb

--- simple_proxy.rb
+++ simple_proxy.rb
@@ -7,9 +7,10 @@ post '/' do
 	method = params[:method] || :get
 	data = params[:data]
 	data = JSON.parse(data) if data.is_a?(String)
-  args = [method, url]
+	args = [method, url]
 	args << data if data
 	puts args
 	res = RestClient.send(*args)
+	content_type(res.headers[:content_type])
 	res.body
 end
