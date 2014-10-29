ef2b7cce45fe7dad7b67d7b1de6aaaf077cdefaf
nomadop <nomadop@gmail.com>
2014-10-29 15:31:13 +0800

    Create center.rb

---
+++ center.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'rest-client'
+require 'redis'
+require 'json'
+
+ProxyRedis = Redis.new(host: 'localhost', port: 6379, db: 5)
+
+post '/' do
+	begin
+		id = Time.now.to_f.to_s
+		ProxyRedis.synchronize do
+			queue = JSON.parse(ProxyRedis.get('queue'))
+			queue << params.merge(id: id)
+			ProxyRedis.set('queue', queue.to_json)
+		end
+		res = nil
+		while res == nil
+			sleep 1
+			res = ProxyRedis.get(id)
+		end
+		ProxyRedis.del(id)
+		res
+	rescue Exception => e
+		headers({system_error: 'true'})
+		e.inspect
+	end
+end
