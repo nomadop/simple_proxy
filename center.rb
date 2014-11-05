4827344bd3d53a36f0882ab21e7a111ddbfbda17
nomadop <nomadop@gmail.com>
2014-11-05 14:16:25 +0800

    Update center.rb

--- center.rb
+++ center.rb
@@ -9,61 +9,83 @@ require './request.rb'
 require './response.rb'
 
 class Connection
-	attr_accessor :socket
+	attr_accessor :socket, :busy, :host, :port
 
 	RedisServer = Redis.new(host: 'localhost', port: 6379, db: 15)
-	@@conn = nil
+	ConnList = []
+	ConnMutex = Mutex.new
 	
 	def initialize host, port
+		@host = host
+		@port = port
 		@socket = TCPSocket.new host, port
-		raise TypeError unless @socket.is_a?(TCPSocket)
+	end
+
+	def connect
+	end
+
+	def disconnect
+		@busy = false
 	end
 
 	def recv
-		msg = socket.recv(1000000000)
-		loop do
-			begin
-				rest = socket.recv_nonblock(1000000000)
-				msg += rest if rest
-			rescue Exception => e
-				break
-			end
-		end
-		msg
+		data = ""
+    loop do
+      begin
+        msg = socket.read_nonblock(100000)
+        data += msg if msg
+        break if data.end_with?('EOF')
+      rescue Errno::EAGAIN => e
+        sleep 0.1
+      rescue EOFError => e
+        data = ""
+        break
+      end
+    end
+    data[0...-3]
 	end
 
 	def send msg
-		socket.send(msg, 0)
+		socket.write(msg)
+		socket.write('EOF')
 	end
 
-	def self.init_conn
-		server = nil
-		RedisServer.synchronize do
-			servers = JSON.parse(RedisServer.get('servers'))
+	def self.new_conn
+		if ConnList.size <= 10
+			servers = JSON.parse(RedisServer.get('servers') || "{}")
 			server = servers.sort_by{|k ,v| v}.first[0]
-			servers[server] += 1
-			RedisServer.set('servers', servers.to_json)
+			host, port = server.split(':')
+			conn = Connection.new(host, port)
+			ConnList << conn
+			conn
+		else
+			sleep 0.1
+			nil
 		end
-		host, port = server.split(':')
-		@@conn = Connection.new(host, port)
 	end
 
 	def self.get_conn
-		@@conn
+		conn = nil
+		while conn == nil
+			ConnMutex.synchronize do
+				conn = ConnList.select{|c| c.busy == false}.shuffle.first
+				conn.busy = true if conn
+			end
+			conn = new_conn unless conn
+		end
+		conn.connect
+		conn
 	end
 end
-
-Connection.init_conn
 
 post '/' do
 	begin
 		st = Time.now
 		id = st.to_f.to_s
 		conn = Connection.get_conn
-		req = Request.new(id, params[:method], params[:url], params[:data])
+		req = Request.new(params[:method], params[:url], params[:data])
 		conn.send(req.to_yaml)
 		msg = conn.recv
-		# p msg
 		res = YAML.load(msg)
 		status(res.status)
 		content_type(res.content_type)
@@ -71,5 +93,7 @@ post '/' do
 	rescue Exception => e
 		headers({system_error: 'true'})
 		{error: e.inspect, backtrace: e.backtrace}.to_json
+	ensure
+		conn.disconnect if conn
 	end
 end
